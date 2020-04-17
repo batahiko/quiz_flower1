1cbc8842aec3be31bd5ad7efc542df4f8caab84e
batahiko <54926029+batahiko@users.noreply.github.com>
2020-04-17 17:17:23 +0900

    Update quizflower1.js

--- quizflower1.js
+++ quizflower1.js
@@ -112,7 +112,7 @@ function explain(answername,number){
                    // ツイートエリアの作成
                 removeAllChildren(tweetDivided);
                 const anchor = document.createElement('a');
-                const hrefValue = <a href="https://twitter.com/intent/tweet?button_hashtag=花の名前当てクイズ&ref_src=twsrc%5Etfw" class="twitter-hashtag-button"  
+                const hrefValue = "https://twitter.com/intent/tweet?button_hashtag=花の名前当てクイズ&ref_src=twsrc%5Etfw"
 
                 anchor.setAttribute('href', hrefValue);
                 anchor.setAttribute('data-url', "https://batahiko.github.io/quizflower1/quizflower1.html")
